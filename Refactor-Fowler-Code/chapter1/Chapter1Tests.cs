8dd4eaee4990657cedd1630cfd0647eec28e5e33
BKSpurgeon <benkoshy@hotmail.com>
2017-06-06 20:31:33 +1000

    add first test

---
+++ Refactor-Fowler-Code/chapter1/Chapter1Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+using RefactorCh1;
+
+namespace Refactor_Fowler_Code.chapter1
+{
+    [TestFixture]
+    class Chapter1Tests
+    {
+
+        [TestCase]
+        public void Statement_EnterMovie_ReturnMovieStatement()
+        {
+            Customer c = new Customer("Freddie");
+            Movie badbois = new Movie("Bad Boyz III", 1);
+            Movie monster = new Movie("Monsters Inc", 2);
+            Movie godfather = new Movie("Monsters Inc", 3);
+
+            Rental r1 = new Rental(badbois, 5);
+            Rental r2 = new Rental(monster, 2);
+            Rental r3 = new Rental(godfather, 1);
+
+            c.addRental(r1);
+            c.addRental(r2);
+            c.addRental(r3);
+
+            string result = c.statement();
+
+        }
+    }
+}
